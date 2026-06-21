0dd682b1556201f1f64ade0991758f95c341ab56
Alain <yinxulai@hotmail.com>
2026-06-21 15:32:56 +0800

    refactor: replace DocShell with Markdown in MDX components
    Rename the exported component from DocShell to Markdown for clearer
    naming convention in the MDX rendering pipeline.

--- packages/renderer/source/mdx/components.ts
+++ packages/renderer/source/mdx/components.ts
@@ -1,17 +1,17 @@
 import { OpenApiPage, ApiEndpoint, OpenApiEndpoint } from '../openapi'
 import { ApiEndpointCard } from '../openapi/components/ApiEndpointCard'
 
-import { DocShell } from './DocShell'
+import { Markdown } from './Markdown'
 import * as mdxPrimitives from './primitives'
 
 export function useMDXComponents(components: Record<string, unknown> = {}) {
   return {
     ...components,
     ...mdxPrimitives,
+    Markdown,
     OpenApiPage,
     ApiEndpoint,
     OpenApiEndpoint,
     ApiEndpointCard,
-    DocShell,
   }
 }
